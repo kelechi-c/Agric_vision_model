2ae28dbb0dfbb120439d1bf6cf1e50fc7b0a0350
Chibuzo Kelechi <narutokelechi@gmail.com>
2024-03-01 05:07:14 +0100

    Aded Gemini API functionality

--- agricImage.py
+++ agricImage.py
@@ -1,28 +1,36 @@
 # Import modules
 import streamlit as st
+import google.generativeai as google_genai
 import numpy as np
 import tracemalloc
 import keras
-
+import os
 from PIL import Image
-tracemalloc.start()
 
+tracemalloc.start()
 
 width = 256
 height = 256
 
+# Gemini API setup
+# GOOGLE_API_KEY=os.getenv('API_KEY')
+GOOGLE_API_KEY='AIzaSyDRkbuyt3ZGehY1wjEDPS9tsft52c0wdIo'
+
+google_genai.configure(api_key=GOOGLE_API_KEY)
+
+gen_model = google_genai.GenerativeModel('gemini-pro-vision')
 
 # Streamlit app/Layout
 
 # Overview
 st.set_page_config(
-    page_title="Agric classification",
+    page_title="Crop image classification",
 )
 
 st.title('Image classification (Crops)')
 
 st.subheader('Overview')
-# st.image('computervision.jpg')
+
 st.markdown('''
          This **machine learning** project is for detecting crop image of the face in the given image .
          It is an application of **computer vision** using Tensorflow. 
@@ -31,7 +39,7 @@ st.markdown('''
 img_path = st.file_uploader('Please upload an image(Face)', type=['png','jpg', 'jpeg'])
 
 # load trained model
-pretrained_model_path = 'template_past/Inception-V1.h5'
+pretrained_model_path = 'Inception-V1.h5'
 
 @st.cache_resource
 def loadmodel():
@@ -57,7 +65,14 @@ def predict(imgpath):
     
     return prediction
 
+def gemini_output():
+    img = Image.open(img_path)
+    gemini_response = gen_model.generate_content(["Give agricultural and nutritional info based on this picture. It should include a description of the crop, how to cultivate it and its optimal growth conditions. If its not a crop image, respond saying that its not a crop image", img], stream=True)
+    gemini_response.resolve()
+    
+    return gemini_response.text
 
+    
 
 classes = {0: 'Cherry', 1: 'Coffee-plant', 2: 'Cucumber', 3: 'Fox_nut(Makhana)', 4: 'Lemon', 5: 'Olive-tree',
                6: 'Pearl_millet(bajra)', 7: 'Tobacco-plant', 8: 'almond', 9: 'banana', 10: 'cardamom', 11: 'chilli',
@@ -66,14 +81,15 @@ classes = {0: 'Cherry', 1: 'Coffee-plant', 2: 'Cucumber', 3: 'Fox_nut(Makhana)',
                25: 'sunflower', 26: 'tea', 27: 'tomato', 28: 'vigna-radiati(Mung)', 29: 'wheat'}
 if img_path:
     image_classifier = loadmodel()
-    prediction = predict(img_path)
+
+prediction = predict(img_path)
 
 predicted_class = prediction.argmax()
 certainty = 100 * prediction.max()
 
 
 if st.button('Classify image'):
-    with st.spinner("Clasifying..."):
+    with st.spinner("Classifying..."):
         st.markdown(f'''
             <div style="background-color: black; color: white; font-weight: bold; padding: 1rem; border-radius: 10px;">
             <h4>Results</h4>
@@ -83,30 +99,43 @@ if st.button('Classify image'):
                 </p>
                 ''', unsafe_allow_html=True)
         st.success('Successful')
+        
+gen_button = st.button('Generate Gemini output')
+
+if gen_button:
+    output = gemini_output()
+
+    st.markdown(f'''
+            <div style="background-color: black; color: white; font-weight: bold; padding: 1rem; border-radius: 10px;">
+            <h4>Output</h4>
+            {output}
+            </div>
+                </p>
+                ''', unsafe_allow_html=True)
+    st.success('Successful')
+
+
+
+# Footer
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+
+
+st.markdown("<hr style='border: 1px dashed #ddd; margin: 2rem;'>", unsafe_allow_html=True) #Horizontal line
+
+st.markdown("""
+    <div style="text-align: center; padding: 1rem;">
+        Project by <a href="https://github.com/ChibuzoKelechi" target="_blank" style="color: white; font-weight: bold; text-decoration: none;">
+         kelechi_tensor</a>
+    </div>
     
-st.image(img_path) # type: ignore
+    <div style="text-align: center; padding: 1rem;">
+        Data from <a href="https://kaggle.com" target="_blank" style="color: lightblue; font-weight: bold; text-decoration: none;">
+         Kaggle</a>
+    </div>
+""",
+unsafe_allow_html=True)
 
-
-# # Footer
-# st.write('')
-# st.write('')
-# st.write('')
-# st.write('')
-
-
-# st.markdown("<hr style='border: 1px dashed #ddd; margin: 2rem;'>", unsafe_allow_html=True) #Horizontal line
-
-# st.markdown("""
-#     <div style="text-align: center; padding: 1rem;">
-#         Project by <a href="https://github.com/ChibuzoKelechi" target="_blank" style="color: white; font-weight: bold; text-decoration: none;">
-#          kelechi_tensor</a>
-#     </div>
-    
-#     <div style="text-align: center; padding: 1rem;">
-#         Data from <a href="https://kaggle.com" target="_blank" style="color: lightblue; font-weight: bold; text-decoration: none;">
-#          Kaggle</a>
-#     </div>
-# """,
-# unsafe_allow_html=True)
-
-# # Peace Out :)
+# Peace Out :)
